44312995651a2505b33ab36c35bcee0b2ad92fa3
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2024-02-18 09:49:45 +0100

    Exam delivery

--- alert.sh
+++ alert.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-MSMTP_CONFIG_FILE="/home/bruno/.msmtprc"
+MSMTP_CONFIG_FILE="/home/USER_NAME/.msmtprc"
 
 
 
 
-recipients="TOUR_EMAIL_HERE"
+recipients="YOUR_EMAIL_HERE"
 subject="Training complete!"
 
-echo -e "Subject:$subject\nFrom:email_here\n\nHi! training was completed. Come and join me!" | sudo -u bruno msmtp --file=$MSMTP_CONFIG_FILE -a gmail $recipients
+echo -e "Subject:$subject\nFrom:email_here\n\nHi! training was completed. Come and join me!" | sudo -u USER_NAME msmtp --file=$MSMTP_CONFIG_FILE -a gmail $recipients
 
 exit 0
 
